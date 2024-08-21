9069f141e213c8fe9c09d7a37c90910dd7e3c882
Haiqastackdeveloper <haiqa0021@gmail.com>
2024-08-21 21:54:34 +0500

    done

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,9 +1,9 @@
-var a= 3;
-var b= 5;
+var a= 4;
+var b= 8;
 document.write("<h1> before swapping the variable a contain "+a+"</h1>");
 document.write("<h1> before swapping the variable b contain "+b+"</h1>");
-a=b;
+a=a+b;
+b=a-b;
+a=a-b;
 document.write("<h1> after swapping the variable a contain "+a+"</h1>");
-var a=3, b=5;
-b=a
 document.write("<h1> after swapping the variable b contain "+b+"</h1>");
